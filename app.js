dbaab22c41646ae340ef927a962506ae20e08c5b
innovator-j <innovatorj01@gmail.com>
2025-02-10 00:23:46 +0100

    testing out

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
 
+// routes
+app.get('/hello', (req, res) => {
+    res.send('Task Manager App')
+})
 
 const port = 3000;
+
+app.listen(port, console.log(`Server is running on port ${port}...`) );
